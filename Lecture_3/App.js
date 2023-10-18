7930d0c2ea5c047209ec40d06e892eed028d5a46
Anshuman Shukla <ansh212039@gmail.com>
2023-10-19 03:28:44 +0530

    Half lecture_3

--- Lecture_3/App.js
+++ Lecture_3/App.js
@@ -1,29 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {
-        id:"title", 
-    },
-    "Welcome"
-)
+// const heading = React.createElement(
+//     "h1",
+//     {
+//         id: "title",
+//         key: "h1",
+//     },
+//     "Welcome"
+// )
 
-const heading2 = React.createElement(
-    "h2",
-    {
-        id:"title2", 
-    },
-    "Helloo"
-)
+// React Component
 
-const container = React.createElement(
-    "div",
-    {
-        id:"div"
-    },
-    [heading,heading2]
-)
+const HeadComp = () => {
+  return (
+    <>
+      <h1>Namaste First Component</h1>
+      <h2>This is h2 to show multiple line example</h2>
+    </>
+  );
+};
+
+// We can skip return as shown below in component
+// const HeadComp2 = () =>(
+//       <>
+//         <h1>Namaste First Component</h1>{" "}
+//         <h2>This is h2 to show multiple line example</h2>
+//       </>
+//     );
+
+// JSX can be used instead of createElement
+const heading = (
+  <>
+    <h1 id="title" key="h1">
+      Welcome
+    </h1>
+    <HeadComp />
+  </>
+);
+
+/* To use element in Component We use Curly braces */
+const HeadComp2 = () => (
+  <>
+    {heading}
+    <h1>Namaste Second Component</h1>
+    {HeadComp()}
+  </>
+//   We can call function in curly braces also bcz at end it is Javascript
+);
+
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "title2",
+//     key: "h2",
+//   },
+//   "Helloo"
+// );
+
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "div",
+//   },
+//   [heading, heading2]
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container); 
+root.render(<HeadComp2 />);
